a9735cc8fe5c340edcae2da5354dd9b1ab9f3ba0
yv0vaa <78468183+yv0vaa@users.noreply.github.com>
2023-12-22 03:53:37 +0300

    Update task11.py

--- task11.py
+++ task11.py
@@ -1,14 +1,34 @@
-import psycopg2
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-conn = psycopg2.connect(dbname='postgres', user='postgres', 
-                        password='16311922', host='localhost')
-cursor = conn.cursor()
-cursor.execute('Select * from db_project.client')
-for elem in cursor.fetchall():
-    print(f'Client\'s full name {elem[1]} {elem[2]}')
+engine = create_engine(f'postgresql://postgres:16311922@localhost/postgres')
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+session = Session()
 
-cursor.execute('select * from db_project.menu_statistics')
-for elem in cursor.fetchall():
-    print(f'{elem[3]} всех потраченных на еду денег в "Капризе" {elem[0]} {elem[1]} спустил на {elem[2]}')
+class Dish(Base):
+    __tablename__ = 'menu'
+    __table_args__ = {'schema': 'db_project'}
 
-#TODO: what to do?
+    dish_id = Column(Integer, primary_key=True)
+    dish_name = Column(String)
+    description = Column(String)
+    price = Column(Integer)
+
+#create
+new_dish = Dish(dish_name='Кровавая Мэри', description='Водка с томатным соком и специями', price = 1000)
+session.add(new_dish)
+session.commit()
+
+#read
+for dish in session.query(Dish).filter(Dish.price > 1000).all():
+    print(f'{dish.dish_name} стоит {dish.price}')
+    
+#update
+dish = session.query(Dish).filter(Dish.dish_name == 'Кровавая Мэри').first()
+dish.price += 500
+session.commit()
+
+# delete
+session.delete(session.query(Dish).filter(Dish.dish_name == 'Кровавая Мэри').first())
+session.commit()
